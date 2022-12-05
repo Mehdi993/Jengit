eda0bc20916f701220d1328a02b426c61509d5be
Mehdi <mehdi.mhedhbi993@gmail.com>
2022-12-05 15:38:11 +0100

    adding the project files

---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Starting Runnig maven proejct ");
+		System.out.println("Ending maven project ");
+
+
+	}
+
+}
